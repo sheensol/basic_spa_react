3230aed97dce391c38c3af2dd89050adc874f4cc
Imran Haider <sheensoltech@gmail.com>
2022-12-14 21:14:18 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
+import React from 'react'
+
+import { Route, Routes } from 'react-router-dom'
+
+// We will create these two pages in a moment
+
+import HomePage from './pages/HomePage'
+
+import UserPage from './pages/UserPage'
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
 
-export default App;
+  return (
+    
+
+    <Routes>
+
+<Route exact path='/' element={<HomePage/>} />
+
+<Route path='/:id' element={<UserPage/>} />
+
+</Routes>
+
+  )
+
+}
+ 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { render } from 'react-dom'
+
+
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+// Bootstrap Bundle JS
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+
+render(
+
+  <BrowserRouter>
+
     <App />
-  </React.StrictMode>
-);
+
+  </BrowserRouter>,
+
+  document.querySelector('#root')
+
+)
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
